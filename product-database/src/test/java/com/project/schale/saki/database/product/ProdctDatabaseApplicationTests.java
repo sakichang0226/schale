6b39b2e9bdb4b6f2c1245c740156d0632971ad0b
sakichang0226 <saki.develop0226@gmail.com>
2024-10-20 22:47:30 +0900

    feat: 環境構築

---
+++ product-database/src/test/java/com/project/schale/saki/database/product/ProdctDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.schale.saki.database.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdctDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
