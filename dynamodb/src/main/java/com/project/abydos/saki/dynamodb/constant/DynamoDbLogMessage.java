72036862c4906d4b3705647d6e1189e0f3d9888d
sakichang0226 <saki.develop0226@gmail.com>
2026-07-14 00:35:17 +0900

    DynamoDBのリクエスト・レスポンスをログで追えるようにする

---
+++ dynamodb/src/main/java/com/project/abydos/saki/dynamodb/constant/DynamoDbLogMessage.java
@@ -0,0 +1,25 @@
+package com.project.abydos.saki.dynamodb.constant;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+/**
+ * DynamoDBパッケージのログメッセージ定数.
+ */
+@Getter
+@RequiredArgsConstructor
+public enum DynamoDbLogMessage {
+
+    /**
+     * Dynamo DBテーブルへのリクエスト時のログ
+     */
+    REQUEST("Request={}"),
+
+    /**
+     * Dynamo DBテーブルのレスポンスログ
+     */
+    RESPONSE("Request={}");
+
+    private final String message;
+
+}
